24a66e7baac99904356c8d849d69f16513a7f24f
louis61619 <louis61619@gmail.com>
2020-07-21 02:30:35 +0800

    add goodslist

--- src/components/content/tabControl/TabControl.vue
+++ src/components/content/tabControl/TabControl.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="tab-control">
-    <div v-for="item in titles" :key="item.index" class="tab-control-item">
-      {{item}}
+    <div v-for="(item, index) in titles" :key="item.index"
+     class="tab-control-item" :class="{active: index === currentIndex}"
+      @click="itemClick(index)">
+     <span>{{item}}</span>
+      
     </div>
   </div>
 </template>
@@ -16,12 +19,22 @@ export default {
         return []
       }
     }
+  },
+  data() {
+    return {
+      currentIndex: 0
+    }
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index
+    }
   }
 
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   .tab-control{
     width: 100%;
     display: flex;
@@ -29,10 +42,19 @@ export default {
       flex: 1;
       text-align: center;
       height: 40px;
+      line-height: 40px;
       font-size: 15px;
+      span{
+        padding-bottom: 5px;
+        padding-right: 5px;
+        padding-left: 5px;
+      }
     }
   }
   .active {
-    color: var(--color-high-text)
+    color: var(--color-high-text);
+    span{
+      border-bottom: 3px solid var(--color-tint);
+    }
   }
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,86 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <nav-bar class="home-nav"><div slot="center">購物街</div></nav-bar>
     <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends"/> 
     <feature-view></feature-view>
-    <tab-control :titles="['流行','新款','精選']"></tab-control>
+    <tab-control :titles="['流行','新款','精選']" class="tab-control"></tab-control>
+    <goods-list :goods="goods['pop'].list"></goods-list>
+    <ul>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+      <li>內容</li>
+    </ul>
+    
   </div>
 </template>
 
@@ -16,8 +92,9 @@ import FeatureView from './chidComps/FeatureView'
 
 import NavBar from 'components/common/navbar/NavBar'
 import TabControl from 'components/content/tabControl/TabControl'
+import GoodsList from 'components/content/goods/GoodsList'
 
-import {getHomeMultidata} from 'network/home'
+import {getHomeMultidata, getHomeGoods} from 'network/home'
 
 
 export default {
@@ -29,32 +106,60 @@ export default {
     FeatureView,
     NavBar,
     TabControl,
+    GoodsList
 
   },
   data() {
     return {
       // result: null,
       banners: [],
-      recommends: []
+      recommends: [],
+      goods: {
+        'pop': {page: 0, list: []},
+        'new': {page: 0, list: []},
+        'sell': {page: 0, list: []}
+      }
     }
   },
   created() {
     //1.請求多個數據
-    getHomeMultidata().then(res => {
+    this.getHomeMultidata()
+
+    //2.請求商品數據
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
       // console.log(res.data) //垃圾回收機制所以要放入data中
       // this.result = res.data
       this.banners = res.data.banner.list;
       this.recommends = res.data.recommend.list;
 
-    })
+      })
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1 //請求時傳遞參數
+      getHomeGoods('pop', page).then(res => {
+        console.log(res)
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page += 1 //改變vuedata資料
+      })
+    }
   }
 }
 </script>
 
 <style>
+  html,body{
+    width: 100%
+  }
   #home{
     padding-top: 44px;
-    padding-bottom: 100px;
+    
+    width: 100%;
   }
   .home-nav{
     background-color: var(--color-tint);
@@ -66,5 +171,12 @@ export default {
     right: 0;
     z-index: 9;
 
+  }
+  .tab-control{
+    position: -webkit-sticky;
+    position: sticky;
+    top: 44px;
+    background: #fff;
+    z-index: 2;
   }
 </style>
