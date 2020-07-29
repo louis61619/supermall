0f2bbe064c7d1a0bc25955633c46c5f7542f00a0
louis61619 <louis61619@gmail.com>
2020-07-29 23:01:56 +0800

    新增 滾動與選單

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,13 +1,143 @@
 <template>
-  <h2>分類</h2>
+  <div class="wrapper">
+    <ul class="content">
+      <li>分類列表1</li>
+      <li>分類列表2</li>
+      <li>分類列表3</li>
+      <li>分類列表4</li>
+      <li>分類列表5</li>
+      <li>分類列表6</li>
+      <li>分類列表7</li>
+      <li>分類列表8</li>
+      <li>分類列表9</li>
+      <li>分類列表10</li>
+      <li>分類列表11</li>
+      <li>分類列表12</li>
+      <li>分類列表13</li>
+      <li>分類列表14</li>
+      <li>分類列表15</li>
+      <li>分類列表16</li>
+      <li>分類列表17</li>
+      <li>分類列表18</li>
+      <li>分類列表19</li>
+      <li>分類列表20</li>
+      <li>分類列表21</li>
+      <li>分類列表22</li>
+      <li>分類列表23</li>
+      <li>分類列表24</li>
+      <li>分類列表25</li>
+      <li>分類列表26</li>
+      <li>分類列表27</li>
+      <li>分類列表28</li>
+      <li>分類列表29</li>
+      <li>分類列表30</li>
+      <li>分類列表31</li>
+      <li>分類列表32</li>
+      <li>分類列表33</li>
+      <li>分類列表34</li>
+      <li>分類列表35</li>
+      <li>分類列表36</li>
+      <li>分類列表37</li>
+      <li>分類列表38</li>
+      <li>分類列表39</li>
+      <li>分類列表40</li>
+      <li>分類列表41</li>
+      <li>分類列表42</li>
+      <li>分類列表43</li>
+      <li>分類列表44</li>
+      <li>分類列表45</li>
+      <li>分類列表46</li>
+      <li>分類列表47</li>
+      <li>分類列表48</li>
+      <li>分類列表49</li>
+      <li>分類列表50</li>
+      <li>分類列表51</li>
+      <li>分類列表52</li>
+      <li>分類列表53</li>
+      <li>分類列表54</li>
+      <li>分類列表55</li>
+      <li>分類列表56</li>
+      <li>分類列表57</li>
+      <li>分類列表58</li>
+      <li>分類列表59</li>
+      <li>分類列表60</li>
+      <li>分類列表61</li>
+      <li>分類列表62</li>
+      <li>分類列表63</li>
+      <li>分類列表64</li>
+      <li>分類列表65</li>
+      <li>分類列表66</li>
+      <li>分類列表67</li>
+      <li>分類列表68</li>
+      <li>分類列表69</li>
+      <li>分類列表70</li>
+      <li>分類列表71</li>
+      <li>分類列表72</li>
+      <li>分類列表73</li>
+      <li>分類列表74</li>
+      <li>分類列表75</li>
+      <li>分類列表76</li>
+      <li>分類列表77</li>
+      <li>分類列表78</li>
+      <li>分類列表79</li>
+      <li>分類列表80</li>
+      <li>分類列表81</li>
+      <li>分類列表82</li>
+      <li>分類列表83</li>
+      <li>分類列表84</li>
+      <li>分類列表85</li>
+      <li>分類列表86</li>
+      <li>分類列表87</li>
+      <li>分類列表88</li>
+      <li>分類列表89</li>
+      <li>分類列表90</li>
+      <li>分類列表91</li>
+      <li>分類列表92</li>
+      <li>分類列表93</li>
+      <li>分類列表94</li>
+      <li>分類列表95</li>
+      <li>分類列表96</li>
+      <li>分類列表97</li>
+      <li>分類列表98</li>
+      <li>分類列表99</li>
+      <li>分類列表100</li>
+    </ul>
+  </div>
+  
 </template>
 
 <script>
-export default {
+import BScroll from 'better-scroll'
 
+export default {
+  name: "Category",
+  data() {
+    return {
+      scroll: null
+    }
+  },
+  mounted() {
+    let wrapper = document.querySelector('.wrapper')
+    this.scroll = new BScroll(wrapper, {
+      probeType: 3,
+      pullUpLoad: true
+    })
+    this.scroll.on('scroll', (position) => {
+      console.log(position)
+    })
+    this.scroll.on('pullingUp', () => {
+      console.log('加載更多')
+    })
+  },
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .wrapper{
+    height: 300px;
+    background: chartreuse;
 
+    // overflow: hidden;
+    // overflow-y: scroll;
+  }
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,113 +1,45 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav"><div slot="center">購物街</div></nav-bar>
-    <home-swiper :banners="banners"/>
-    <recommend-view :recommends="recommends"/> 
-    <feature-view></feature-view>
-    <tab-control :titles="['流行','新款','精選']" class="tab-control"></tab-control>
-    <goods-list :goods="goods['pop'].list"></goods-list>
-    <ul>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
+    <nav-bar class="home-nav">
+      <div slot="center">購物街</div>
+    </nav-bar>
 
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-      <li>內容</li>
-    </ul>
-    
+    <scroll class="content" ref="scroll">
+      <home-swiper :banners="banners" />
+      <recommend-view :recommends="recommends" />
+      <feature-view></feature-view>
+      <tab-control :titles="['流行','新款','精選']" class="tab-control" @tabClick="tabClick"></tab-control>
+      <goods-list :goods="showGoods"></goods-list>
+    </scroll>
+
+    <back-top @click.native="backClick"></back-top>
   </div>
 </template>
 
 <script>
+import HomeSwiper from "./chidComps/HomeSwiper";
+import RecommendView from "./chidComps/RecommendView";
+import FeatureView from "./chidComps/FeatureView";
 
-import HomeSwiper from './chidComps/HomeSwiper'
-import RecommendView from './chidComps/RecommendView'
-import FeatureView from './chidComps/FeatureView'
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
+import GoodsList from "components/content/goods/GoodsList";
+import Scroll from "components/common/scroll/Scroll";
+import BackTop from "components/content/backTop/BackTop"
 
-import NavBar from 'components/common/navbar/NavBar'
-import TabControl from 'components/content/tabControl/TabControl'
-import GoodsList from 'components/content/goods/GoodsList'
-
-import {getHomeMultidata, getHomeGoods} from 'network/home'
-
+import { getHomeMultidata, getHomeGoods } from "network/home";
 
 export default {
   name: "Home",
   components: {
-    
     HomeSwiper,
     RecommendView,
     FeatureView,
     NavBar,
     TabControl,
-    GoodsList
-
+    GoodsList,
+    Scroll,
+    BackTop
   },
   data() {
     return {
@@ -115,68 +47,102 @@ export default {
       banners: [],
       recommends: [],
       goods: {
-        'pop': {page: 0, list: []},
-        'new': {page: 0, list: []},
-        'sell': {page: 0, list: []}
-      }
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
+      },
+      currentType: "pop"
+    };
+  },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list;
     }
   },
   created() {
     //1.請求多個數據
-    this.getHomeMultidata()
+    this.getHomeMultidata();
 
     //2.請求商品數據
-    this.getHomeGoods('pop')
-    this.getHomeGoods('new')
-    this.getHomeGoods('sell')
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
   },
   methods: {
+    /**
+     * 事件監聽相關的方法
+     */
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+          break;
+      }
+    },
+    backClick() {
+      // this.$refs.srcoll.scroll.scrollTo(0 , 0)
+      this.$refs.scroll.scrollTo(0, 0, 500)
+      // console.log(this.$refs.scroll.scroll.scrollTo)
+    },
+    /**
+     * 網路請求相關的方法
+     */
     getHomeMultidata() {
       getHomeMultidata().then(res => {
-      // console.log(res.data) //垃圾回收機制所以要放入data中
-      // this.result = res.data
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-
-      })
+        // console.log(res.data) //垃圾回收機制所以要放入data中
+        // this.result = res.data
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
     },
     getHomeGoods(type) {
-      const page = this.goods[type].page + 1 //請求時傳遞參數
-      getHomeGoods('pop', page).then(res => {
-        console.log(res)
-        this.goods[type].list.push(...res.data.list)
-        this.goods[type].page += 1 //改變vuedata資料
-      })
+      const page = this.goods[type].page + 1; //請求時傳遞參數
+      getHomeGoods("pop", page).then(res => {
+        // console.log(res)
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1; //改變vuedata資料
+      });
     }
   }
-}
+};
 </script>
 
-<style>
-  html,body{
-    width: 100%
-  }
-  #home{
-    padding-top: 44px;
-    
-    width: 100%;
-  }
-  .home-nav{
-    background-color: var(--color-tint);
-    color: #fff;
+<style scoped>
+html,
+body {
+  width: 100%;
+  
+}
+#home {
+  padding-top: 44px;
+  height: 100vh;
+  width: 100%;
+}
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
 
-    position: fixed;
-    left: 0;
-    top: 0;
-    right: 0;
-    z-index: 9;
-
-  }
-  .tab-control{
-    position: -webkit-sticky;
-    position: sticky;
-    top: 44px;
-    background: #fff;
-    z-index: 2;
-  }
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  z-index: 9;
+}
+.tab-control {
+  position: -webkit-sticky;
+  position: sticky;
+  top: 44px;
+  background: #fff;
+  z-index: 2;
+}
+.content{
+  height: calc(100% - 49px);
+  overflow: hidden;
+}
 </style>
